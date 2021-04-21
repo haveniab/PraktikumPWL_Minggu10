ac0fd78d4157c6b51e28093e6988c3360b227d5d
haveniab <havenia09@gmail.com>
2021-04-22 00:40:30 +0700

    perubahan kode program pada ArticleController

--- app/Http/Controllers/ArticleController.php
+++ app/Http/Controllers/ArticleController.php
@@ -109,4 +109,9 @@ class ArticleController extends Controller
         //
     }
 
+    public function cetak_pdf(){
+        $articles = Article::all();
+        $pdf = PDF::loadview('articles.articles_pdf', ['articles'=>$articles]);
+        return $pdf->stream();
+    }
 }
