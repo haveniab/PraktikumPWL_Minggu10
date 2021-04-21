3ac0edb0b13a68f57f32291884c0364694cd66df
haveniab <havenia09@gmail.com>
2021-04-22 00:23:46 +0700

    menambahkan package DomPDF

--- config/app.php
+++ config/app.php
@@ -166,6 +166,8 @@ return [
          * Package Service Providers...
          */
 
+        Barryvdh\DomPDF\ServiceProvider::class,
+
         /*
          * Application Service Providers...
          */
@@ -228,6 +230,8 @@ return [
         'Validator' => Illuminate\Support\Facades\Validator::class,
         'View' => Illuminate\Support\Facades\View::class,
 
+        'PDF' => Barryvdh\DomPDF\Facade::class,
+    
     ],
 
 ];
