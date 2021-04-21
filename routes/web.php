6462c4524d8beee1fd2d08a012e901727c862a04
haveniab <havenia09@gmail.com>
2021-04-21 23:54:23 +0700

    fix controller, route, dan view create

--- routes/web.php
+++ routes/web.php
@@ -18,8 +18,9 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-Route::resource('articles', ArticleController::class);
+
 
 Auth::routes();
 
-Route::get('/articles', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+Route::resource('articles', ArticleController::class);
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
